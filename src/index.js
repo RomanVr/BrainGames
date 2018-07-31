4a8bfd1ec3f62c55758f3c23622409cbffdc62ea
Roman Vr <romakf99@yandex.ru>
2018-08-01 00:59:13 +0300

    Added Eslint and fix 'problem-eslint'

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const actual = readlineSync.question('May I have your name? ');
   console.log('\n');
-  console.log('Hello, ' + actual + '!');
-}
+  console.log(`Hello, ${actual}`);
+};
 export default greeting;
